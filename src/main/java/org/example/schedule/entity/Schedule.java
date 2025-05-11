94df5caaf7e3fa5ffd43013a38f4ae8f8370a29b
김민균 <kmg02845@gmail.com>
2025-05-11 20:38:03 +0900

    set Schedule.java

--- src/main/java/org/example/schedule/entity/Schedule.java
+++ src/main/java/org/example/schedule/entity/Schedule.java
@@ -1,4 +1,12 @@
 package org.example.schedule.entity;
 
+import java.time.LocalDateTime;
+
 public class Schedule {
+    int id;
+    String task;
+    String user_name;
+    String password;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
